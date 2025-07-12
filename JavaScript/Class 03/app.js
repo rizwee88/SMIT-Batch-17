55bcf33f0ba03a4d7b68cf5adde3784062a980e2
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-07-12 09:53:28 +0500

    Post/Pre Increment&Decrement

---
+++ JavaScript/Class 03/app.js
@@ -0,0 +1,19 @@
+alert("Hello Students");
+
+// var num = 10;
+
+// num + 1;
+// num++; => Increment
+// num--; => Dcrement
+
+// console.log(num);
+
+// Pre Increment / Post Incremnent
+
+// num++; => Post Increment
+// ++num; => Pre Increment
+
+// Pre Decrement / Post Decremnent
+
+// num--; => Post Decrement
+// --num; => Pre Decrement
