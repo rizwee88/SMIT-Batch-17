bfab786ae17b7fad48d7c728ae40cf66c1b4b477
AdilAhmedShekhani <adilaziz2009@live.com>
2025-07-22 20:31:21 +0500

    Falsy Values

--- JavaScript/Class 07/app.js
+++ JavaScript/Class 07/app.js
@@ -1,2 +1,11 @@
 console.log("Hello Students");
 
+//  Falsy Value (6 types) other than this values are true
+
+// "" Empty String ==> False
+// 0 ==> False
+// false ==> False
+// undefind ==> False
+//NaN ==> False
+// null ==> False
+
