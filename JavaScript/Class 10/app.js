dcc2ddbbabe80068ffcf26c4501d996ef1f3a732
AdilAhmedShekhani <adilaziz2009@live.com>
2025-08-03 20:42:41 +0500

    Splice Method

--- JavaScript/Class 10/app.js
+++ JavaScript/Class 10/app.js
@@ -1,2 +1,12 @@
 console.log("Hello Students");
 
+// Splice Method
+// Delete Elements and Add Elements
+// First Target and how many elements deleted
+// also add Elements
+
+var pets = ["dog", "cat", "fly", "bug", "ox"]
+
+pets.splice(2, 2, "pig", "duck", "emu");
+
+console.log(pets);
