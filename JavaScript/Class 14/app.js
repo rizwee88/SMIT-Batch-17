e8b075ef7fb7b12ae995699381608f27f3198076
AdilAhmedShekhani <adilaziz2009@live.com>
2025-08-14 22:43:14 +0500

    Replace Methods

--- JavaScript/Class 14/app.js
+++ JavaScript/Class 14/app.js
@@ -21,3 +21,16 @@ if (text.indexOf("Pakistan") !== -1) {
 } else {
     console.log("Nhi Mila")
 }
+
+
+// Replace Method
+
+// var str = "Adil Ahmed"
+
+// console.log(str.replace("Ahmed", "Khan"))
+
+// Multiple Replace 
+
+var text = "The IT industry in Pakistan is  Dollar experiencing rapid growth, contributing significantly to the national economy and demonstrating potential Dollar for further expansion. It's a key sector for exports and job creation, with a large and growing pool of young, tech-savvy professionals. "
+
+console.log(text.replaceAll("Dollar", "Euro"))
