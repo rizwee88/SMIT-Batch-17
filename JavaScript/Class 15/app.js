e0dfff8a45127445797d3a79382a0d61f9dfe2d3
AdilAhmedShekhani <adilaziz2009@live.com>
2025-08-25 22:12:48 +0500

    Math Random & Floor Method

--- JavaScript/Class 15/app.js
+++ JavaScript/Class 15/app.js
@@ -1,1 +1,11 @@
 alert("Hello")
+
+var randomNumber = Math.random();
+
+console.log(randomNumber)
+
+var bigDecimal = Math.random();
+var improvedNum = (bigDecimal * 6) + 1;
+var numberOfStars = Math.floor(improvedNum);
+
+console.log(numberOfStars)
