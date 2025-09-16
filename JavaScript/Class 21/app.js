787dfaf1f9aa92ca68bc5cfa9271250650ce7a26
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-09-16 09:15:31 +0500

    Switch Statement

--- JavaScript/Class 21/app.js
+++ JavaScript/Class 21/app.js
@@ -1,1 +1,26 @@
 console.log("Hello");
+
+// Conditional Statement
+
+if (dayOfWk === "Sat" || dayOfWk === "Sun") {
+  alert("Whoopee!");
+} else if (dayOfWk === "Fri") {
+  alert("TGIF!");
+} else {
+  alert("Shoot me now!");
+}
+
+// Switch Statement
+switch (dayOfWk) {
+  case "Sat":
+    alert("Whoopee");
+    break;
+  case "Sun":
+    alert("Whoopee");
+    break;
+  case "Fri":
+    alert("TGIF!");
+    break;
+  default:
+    alert("Shoot me now!");
+}
