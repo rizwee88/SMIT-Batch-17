1216e8dbba70af4afdedc79993a7d8515b85ada9
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-09-27 10:12:21 +0500

    do while loop

--- JavaScript/Class 23/app.js
+++ JavaScript/Class 23/app.js
@@ -1,10 +1,18 @@
 console.log("Hello");
 
-// While Loops 
+// While Loops
 
 var i = 0;
 while (i <= 3) {
   alert(i);
   i++;
 }
+
+// do while loop (it always executes that code at least once, no matter what the looplimiter says.)
+
+var i = 0;
+do {
+  alert(i);
+  i++;
+} while (i <= 3);
 
