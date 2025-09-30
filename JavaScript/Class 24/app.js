efab486ce1a9c466be8e6bee891989d5d5e84ba4
AdilAhmedShekhani <adilaziz2009@live.com>
2025-09-30 22:06:20 +0500

    For of Loop

--- JavaScript/Class 24/app.js
+++ JavaScript/Class 24/app.js
@@ -6,3 +6,14 @@ function submit() {
     image.src = URL.createObjectURL(file.files[0]);
     image.style.display = "block";
 }
+
+// For of Loop
+
+function getValue() {
+    var cities = document.getElementsByName("city");
+    for (var city of cities) {
+        if (city.checked) {
+            console.log(city.value)
+        }
+    }
+}
