2b39d8b5e6d12b486ef10bc850b11ea2762f54fc
AdilAhmedShekhani <adilaziz2009@live.com>
2025-10-13 21:36:23 +0500

    Add Event Listner

--- JavaScript/Class 27/app.js
+++ JavaScript/Class 27/app.js
@@ -1,1 +1,22 @@
 console.log("Hello")
+
+// Add Event Listner (Click)
+
+var btn = document.getElementById("myBtn");
+
+
+btn.addEventListener("click", function () {
+    alert("Button Clicked!");
+});
+
+// Add Event Listner (Mouse Over and Out)
+
+var text = document.getElementById("demo");
+
+text.addEventListener("mouseover", function () {
+    text.style.color = "blue";
+});
+
+text.addEventListener("mouseout", function () {
+    text.style.color = "black";
+});
