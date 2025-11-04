56be429e21a88aaebc30cc117c9d4d538f0c5168
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-11-04 10:02:37 +0500

    Objects Constructors

--- JavaScript/Class 33/app.js
+++ JavaScript/Class 33/app.js
@@ -49,3 +49,17 @@ var plan1 = {
     return bestPrice * 12;
   },
 };
+
+// Object Constructors
+
+function Plan(name, price, space, transfer, pages) {
+  this.name = name;
+  this.price = price;
+  this.space = space;
+  this.transfer = transfer;
+  this.pages = pages;
+}
+
+var plan1 = new Plan("Basic", 3.99, 100, 1000, 10);
+
+console.log(plan1);
