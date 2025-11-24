547091f9cdf3c6a5148ed5cde2a3393aa101c292
AdilAhmedShekhani <adilaziz2009@live.com>
2025-11-24 21:39:22 +0500

    Var / Let / Const

---
+++ Advance Javascript/Class 01/app.js
@@ -0,0 +1,17 @@
+// Var have Global Scope
+
+var name = "Adil";
+var name = "Ahmed"; 
+console.log(name);
+
+// Let have Blocked Scope
+
+let age = 20;
+age = 21;
+console.log(age);
+
+// Const Used for values that should not change.
+
+const country = "Pakistan";
+console.log(country);
+
