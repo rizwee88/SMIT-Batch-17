50ee0c4b104cb61e144f7c02e2b6af2479b88b31
Your Namerizwee88 <your_email@example.comrizwee88@gmail.com>
2025-12-13 20:36:57 +0500

    I create Palindrome  from loop

--- Advance Github Assignment/app.js
+++ Advance Github Assignment/app.js
@@ -1,4 +1,25 @@
-// Write a Statemnet of Palindrom without using any method
-// Create Pull reques then add your code
-// Your Code Here 
+var text = prompt("Type a word or phrase to check if it is a palindrome or not")
 
+
+var cleaned = text.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
+
+var ispalindrome = true;
+
+for(i = 0; i < Math.floor(cleaned.length / 2); i++){
+    if(cleaned[i] !== cleaned[cleaned.length -1 -i]){
+        ispalindrome = false;
+        break;
+    }
+}
+if(cleaned.length === 0){
+
+    document.write("Blank or invalid input — Write a correct word or phrase.")
+
+} else if(ispalindrome){
+
+    document.write(`"${text}"</br> This is a Palindrome.`);
+}
+else{
+
+    document.write(`"${text}"</br> This is Not a Palindrome.`);
+}
