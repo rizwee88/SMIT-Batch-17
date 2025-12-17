208d4d0a0b4e1f6650a17cd2f19594329dd2dc92
AdilAhmedShekhani <adilaziz2009@live.com>
2025-12-17 20:26:31 +0500

    First Class Function

--- Advance Javascript/Class 05/app.js
+++ Advance Javascript/Class 05/app.js
@@ -9,3 +9,34 @@ console.log(num)
 let num = Math.pow(2, 4)
 
 console.log(num)
+
+// First Class Function
+
+const sayHello = function () {
+    console.log("Hello");
+};
+
+sayHello();
+
+// Function Passed an Arguments
+
+function greet(fn) {
+    fn();
+}
+
+function hello() {
+    console.log("Hello World");
+}
+
+greet(hello);
+
+// Function returned from another function
+
+function outer() {
+    return function inner() {
+        console.log("Inner Function");
+    };
+}
+
+const myFunc = outer();
+myFunc();
