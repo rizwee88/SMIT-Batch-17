28aade8d5b5948fbceaa1ea8cd297fbd964c4125
AdilAhmedShekhani <adilaziz2009@live.com>
2025-12-17 20:32:21 +0500

    Classes

---
+++ Advance Javascript/Class 06/app.js
@@ -0,0 +1,12 @@
+// Javascript OOP Concepts
+
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  study() {
+    console.log(this.name + " is studying");
+  }
+}
