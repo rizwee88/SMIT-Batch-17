ae08ae112929b452332f3410a19fe6ad85877731
Your Namerizwee88 <your_email@example.comrizwee88@gmail.com>
2025-12-18 19:27:47 +0500

    i update app.js please check

--- Advance Github Assignment/app.js
+++ Advance Github Assignment/app.js
@@ -2,3 +2,34 @@
 // Create Pull reques then add your code
 // Your Code Here 
 
+// Create a Set
+const letters = new Set();
+
+// Create Variables
+const a = "a";
+const b = "b";
+const c = "c";
+
+// Add Variables to the Set
+letters.add(a);
+letters.add(b);
+letters.add
+
+//=============================================================
+
+
+// Create an empty Map
+const fruits = new Map();
+
+// Set Map Values
+fruits.set("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);
+
+
+// Create a Map
+const fruits2 = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
